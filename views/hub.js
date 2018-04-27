a157f9324cc85837bd0a1fd708bd16db495297f9
Lei0911 <zhulei91225@hotmail.com>
2018-04-27 16:21:28 -0700

    modified chat.html hub.css hub.js

---
+++ views/hub.js
@@ -0,0 +1,48 @@
+var crNum1 = document.getElementById("chatRoom1"),
+	crNum2 = document.getElementById("chatRoom2"),
+	crNum3 = document.getElementById("chatRoom3"),
+	ur1Text = document.getElementById("user1_text"),
+	ur2Text = document.getElementById("user2_text"),
+	ur3Text = document.getElementById("user3_text"),
+	sdB = document.getElementById("sendBtn"),
+	nChtRoom = document.getElementById("addCrButton"),
+	chtDisp = document.getElementById("chatDisplay"),
+	addChtRmPupPg = document.getElementById("addChatRoomPopUpPage"),
+	createBtn = document.getElementById("createButton");
+
+crNum1.addEventListener("click",function(){
+	ur3Text.innerHTML = "This is User3's Text"
+	ur2Text.innerHTML = "This is User2's Text"
+	ur1Text.innerHTML = "This is User1's Text"
+});
+crNum2.addEventListener("click",function(){
+	ur3Text.innerHTML = ""
+	ur2Text.innerHTML = ""
+	ur1Text.innerHTML = ""
+});
+crNum3.addEventListener("click",function(){
+	ur3Text.innerHTML = ""
+	ur2Text.innerHTML = ""
+	ur1Text.innerHTML = ""
+});
+sdB.addEventListener("click",function(){
+	alert("Testing Send Button  !!!")
+});
+nChtRoom.addEventListener("click",function(){
+	addChtRmPupPg.style.display = "block";
+});
+createBtn.addEventListener("click",function(){
+	alert("Add Person to Chat room !!!")
+});
+
+
+
+
+
+
+
+
+
+
+
+
