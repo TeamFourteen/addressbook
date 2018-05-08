ff6a6942b1fae9fda96ede529f9289d134362e0b
Lei0911 <zhulei91225@hotmail.com>
2018-05-08 16:38:11 -0700

    edit server_tutorial

--- views/chatroom/server_tutorial/server.js
+++ views/chatroom/server_tutorial/server.js
@@ -1,9 +1,18 @@
 const express = require("express")  //imports express module which helps with making internet connections 
 const app = express()
 const bodyParser = require("body-parser") //helps with retrieveing stuff from database
-const hbs = require("hbs")
+const hbs = require("hbs");
 
-app.use(express.static(__dirname + "/folder"))
+const http = require("http").createServer(app);
+const io = require("socket.io").listen(http);
+
+
+var char_room= ["chatRoom1","chatRoom2","chatRoom3"],
+	user_count = 0;
+	
+
+
+app.use(express.static(__dirname + "/folder")) //<-- css and javascript
 app.use(bodyParser.json()) // this will run first before it sends something
 app.set("view engine", "hbs")
 //--------------------------------------------------------------------
@@ -15,7 +24,10 @@ app.get('/', function(require,response){
     })
     
 }) //This is called a router which helps you get to your page
-
+app.get('/chat',function(require,response){
+	response.sendFile(__dirname + "/chat.html")
+	
+});
 app.post('/item', (require,response)=>{
     console.log(require.body.want)
     response.send({message:"I got them monies"})
@@ -33,14 +45,36 @@ app.get("/message",(require,response)=>{
 	response.send("first message")
     
 });
+//-------------------------------------------------------------------
+var users=[];
+io.on('connection', function(socket){
+	console.log("user connected")
+	user_count +=1
+	socket.emit('userAcount', 'user'+user_count.toString())
+	socket.on("choiceChat", function(data){      //socket.on <--listen 
+		console.log(data)
+		socket.join(data.room)
+		socket.emit('currentRoom', data.room)
+		users.push(data.user)
+		io.in(data.room).emit('joinRoom', users)
+		io.in(data.room).emit('chat', {user:'server', message:'someone has joined this room'}) //anything.emit <-- say something
+	})
+	socket.on("sendMessage", function(data){
+		io.in(data.room).emit('chat', {user:data.user, message:data.message})
+	})
+	socket.on('disconnect', function(){
+		console.log("user disconnected")
+	}); 
+})
 
 //-------------------------------------------------------------------
-app.listen(4500, function(err){
+http.listen(4500, function(err){
 	if(err){
 		console.log(err)
 	}
 	console.log("listening on port 4500")
 })
+
 
 
 
