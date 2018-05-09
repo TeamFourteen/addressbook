c1b1fc97fd79d3bea0fa21d0eaf663f1503c26c5
Michael-Vataga <michael.vataga@gmail.com>
2018-05-08 17:18:08 -0700

    incorporated a little bit of hbs into the new profile page html

--- servertutorial/server.js
+++ servertutorial/server.js
@@ -10,7 +10,7 @@ app.set("view engine", "hbs")
 app.get('/', function(require,response){
     
 	response.render("index.hbs", {
-        stuff:"a message",
+        profileName:"Profile Name",
         phoneNumber: [{phone:"1234567890", name:"John Doe"},{phone:"6045324567", name:"Jane Doe"},{phone:"4145679087", name:"November Falls"}]
     })
     
