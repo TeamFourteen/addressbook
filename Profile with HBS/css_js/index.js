2f9d61d9802ce905314b35465deab7b2fc8fbd53
Michael-Vataga <michael.vataga@gmail.com>
2018-05-10 20:41:25 -0700

    added sending of information to server/database

--- Profile with HBS/css_js/index.js
+++ Profile with HBS/css_js/index.js
@@ -1,17 +1,89 @@
 var addressDiv = document.getElementById("addressDiv");
-var googleMap = document.getElementById("googleMap");
+var addressClass = document.getElementsByClassName("addressClass");
+var contactAdd = document.getElementById("contactAdd");
+var contactInputs = document.getElementById("contactInputs");
+var addressAdd = document.getElementById("addressAdd");
+var addressInputs = document.getElementById("addressInputs");
+var addAddress = document.getElementById("addAddress");
+var addressInput = document.getElementById("addressInput");
+var phoneInput = document.getElementById("phoneInput");
+var addPhone = document.getElementById("addPhone");
+var phoneType = document.getElementById("phoneType");
+var bioAdd = document.getElementById("bioAdd");
+var bioInputs = document.getElementById("bioInputs");
+var addBio = document.getElementById("addBio");
+var bioInput = document.getElementById("bioInput");
 
-addressDiv.addEventListener("click", function(){
-    addressDiv.style.height = "350px";
-    googleMap.style.height = "300px";
+
+
+for(i=0; i < addressClass.length; i++){
+  address = addressClass[i].id
+    document.getElementById(address).addEventListener("click", function(){
+        if(document.getElementById(this.id+"_map").className == "mapHide"){
+          document.getElementById(this.id+"_map").className = "mapShow";  
+        } else {
+            document.getElementById(this.id+"_map").className = "mapHide";
+        }
+    });
+};
+
+bioAdd.addEventListener("click", function(){
+   bioInputs.style.display = "block"; 
 });
 
+addBio.addEventListener("click", function(){
+    fetch('/bio', {
+          method:"POST",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body: JSON.stringify({
+            bio:bioInput.value
+          })
+    }).then((response)=>{
+        return response.json();
+    }).then((json)=>{
+        console.log(json.message)
+    })
+});
 
-/*for(i=0; i < phones.length; i++){
-  phone = phones[i].id
-    document.getElementById(phone).addEventListener("click", function(){
-       document.getElementById(this.id).style.fontSize = "25px";
-        document.getElementById(this.id).style.color = "chartreuse";
-    });
-}; */
+contactAdd.addEventListener("click", function(){
+    contactInputs.style.display = "block";
+});
+
+addPhone.addEventListener("click", function(){
+    fetch('/phones', {
+          method:"POST",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body: JSON.stringify({
+            type:phoneType.value, phone:phoneInput.value
+          })
+    }).then((response)=>{
+        return response.json();
+    }).then((json)=>{
+        console.log(json.message)
+    })
+});
+
+addressAdd.addEventListener("click", function(){
+   addressInputs.style.display = "block"; 
+});
+
+addAddress.addEventListener("click", function(){
+    fetch('/item', {
+          method:"POST",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body: JSON.stringify({
+            "address":addressInput.value
+          })
+    }).then((response)=>{
+        return response.json();
+    }).then((json)=>{
+        console.log(json.message)
+    })
+});
 
--- Profile with HBS/server.js
+++ Profile with HBS/server.js
@@ -13,14 +13,27 @@ app.get('/', function(require,response){
 
         profileName:[{fname:"Jhon", lname:"Doe"}],
         bio:"This is a sample Bio",
-        phoneNumber: [{phone_id:"phone_1", type:"Cell Phone:", phone:"123-123-1234"}, {phone_id:"phone_2", type:"Work Phone:", phone:"123-123-1234"}, {phone_id:"phone_3", type:"Home Phone:", phone:"123-123-1234"}]
+        phoneNumber: [{phone_id:"phone_1", type:"Cell Phone:", phone:"123-123-1234"}, {phone_id:"phone_2", type:"Work Phone:", phone:"123-123-1234"}, {phone_id:"phone_3", type:"Home Phone:", phone:"123-123-1234"}],
+        addresses:[{address_id:"address_1", addressName:"555 Seymour Street", addressMap:"map1"}, {address_id:"address_2", addressName:"3328 Kingsway Avenue", addressMap:"map2"}]
+        
+        
     })
     
 }) //This is called a router which helps you get to your page
 
 app.post('/item', (require,response)=>{
-    console.log(require.body.want)
-    response.send({message:"I got them monies"})
+    console.log(require.body)
+    response.send({message:"I got that message"})
+});
+
+app.post('/phones', (require,response)=>{
+    console.log(require.body)
+    response.send({message:"I got that message"})
+});
+
+app.post('/bio', (require,response)=>{
+    console.log(require.body)
+    response.send({message:"I got that message"})
 });
 
 app.get('/profile', (require,response)=>{
