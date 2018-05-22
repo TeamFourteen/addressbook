0cb0d3914be9767315eba342304140c6eb849e31
SamHadavi <s.hadavi@gmail.com>
2018-05-22 12:07:44 -0700

    some updates merged

--- serverSideFunctions/database_retriever/databaseFunctions.js
+++ serverSideFunctions/database_retriever/databaseFunctions.js
@@ -2,14 +2,33 @@
  * @file This is the database module which the server will use to retrieve data from the database
  * @author Glenn Parale
  */
+
+ /**
+ * @module databaseFunctions
+ * @desc This is the database functions used to retrieve data from the database
+ * @requires pg
+ */
+
+
 const { Pool, Client } = require('pg')
 
-var dbURL = process.env.DATABASE_URL || "postgres://postgres:thegreatpass@localhost:5432/callcenter";
+/**
+ * A variable used to identify which database to use
+ * @var {string} dbURL
+ */
+
+var dbURL = process.env.DATABASE_URL || "postgres://postgres:hadavi@localhost:5432/postgres";
+
 
 const pgpool = new Pool({
     connectionString: dbURL,
 })
-
+/**
+* @callback getLoginData
+* @desc This is a function that retrieves user ID (required for user information needs) as well as the password for confirmation purposes.
+* @param {string} username - Username needed to retrieve the user password and user ID.
+* @returns {Promise} Promise object that contains the user password and user ID
+*/
 var getLoginData = (username) => {
     return new Promise((resolve, reject) => {
         pgpool.query('SELECT password, user_id FROM users WHERE username = $1', [username], (err, res) => {
@@ -22,6 +41,12 @@ var getLoginData = (username) => {
     })
 }
 
+/**
+* @callback getUserData
+* @desc This function retrieves user data to be used in the Profiles tab.
+* @param {number} user_id - User ID required 
+* @returns {Promise} Promise object that contains the user's official name as well as their bio, phone numbers, and addresses
+*/
 var getUserData = (user_id) => {
     return new Promise((resolve, reject) => {
         pgpool.query('SELECT fname, lname, username, bio FROM users WHERE user_id = $1', [user_id], (err, res) => {
@@ -60,7 +85,12 @@ var getUserData = (user_id) => {
         })
     })
 }
-
+/**
+* @callback getContAccount
+* @desc This function searches the database for names or email starting with keyword
+* @param {string} keyword - Keyword needed to search for names
+* @returns {Promise} Promise object that contains the users who have names starting with.
+*/
 var getContAccount = (keyword) => {
     return new Promise((resolve, reject) => {
         searchstuff = keyword + '%'
@@ -74,6 +104,13 @@ var getContAccount = (keyword) => {
         })
     })
 }
+/**
+* @callback getContactAddresses
+* @desc This function searches the database for the addresses of the contact
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @param {number} cont_id - Contact ID to determine the persons contact addresses
+* @returns {Promise} Promise object that contains the contact's addresses.
+*/
 var getContactAddresses = (user_id, cont_id) => {
     return new Promise((resolve, reject) => {
         pgpool.query('SELECT * FROM contact_address WHERE user_id= $1 and cont_id= $2', [user_id, cont_id], function(err, res) {
@@ -86,7 +123,13 @@ var getContactAddresses = (user_id, cont_id) => {
         })
     })
 }
-
+/**
+* @callback getContactPhone
+* @desc This function searches the database for the phone numbers of the contact
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @param {number} cont_id - Contact ID to determine the persons contact phone number
+* @returns {Promise} Promise object that contains the contact's phone numbers.
+*/
 var getContactPhone = (user_id, cont_id) => {
     return new Promise((resolve, reject) => {
         pgpool.query('SELECT p_number, p_type FROM contact_phone WHERE user_id= $1 and cont_id= $2', [user_id, cont_id], function(err, res) {
@@ -99,7 +142,12 @@ var getContactPhone = (user_id, cont_id) => {
         })
     })
 }
-
+/**
+* @callback getContactInfo
+* @desc This function retrieves the contact's personal information
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @returns {Promise} Promise object that contains the contact's personal information
+*/
 var getContactsInfo = (user_id) => {
     return new Promise((resolve, reject) => {
         pgpool.query('SELECT cont_id, user_id ,firstname, lastname, with_account, acct_num, bio FROM contacts WHERE user_id = $1', [user_id], (err, res) => {
@@ -112,7 +160,12 @@ var getContactsInfo = (user_id) => {
 
     })
 }
-
+/**
+* @callback getContInfo
+* @desc This function combines four other functions(getContactsInfo, getContactAddresses, getContactPhone, getUserData) to create the user's contacts information
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @returns {Promise} Promise object that contains the contact's overall information
+*/
 async function getContInfo(user_id) { //A way to call promises "synchronously"
     var x = await getContactsInfo(user_id) //Used the await function for stuff that needs promise pending
     contacts = []
@@ -142,7 +195,13 @@ async function getContInfo(user_id) { //A way to call promises "synchronously"
     }
     return contacts
 }
-
+/**
+* @callback addContactPhone
+* @desc This function adds the contact's phone number to the database
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @param {number} cont_id - Contact ID to to add the number to.
+* @returns {Promise} Promise object that returns the status of the addition
+*/
 var addContactPhone = (cont_id, user_id, phone_number, type) => {
     return new Promise((resolve, reject) => {
         if ((/^[0-9]+$/.test(phone_number)) && (phone_number.length == 10)) {
@@ -159,7 +218,15 @@ var addContactPhone = (cont_id, user_id, phone_number, type) => {
     })
 
 }
-
+/**
+* @callback addContact
+* @desc This function adds the contact to the database under the user's contact lists
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @param {string} fname - Contact's First Name
+* @param {string} lname - Contact's Last Name
+* @param {string} bio - Contact's Biography and/or Comments
+* @returns {Promise} Promise object that returns the status of the addition
+*/
 var addContact = (user_id, fname, lname, bio) => {
     return new Promise((resolve, reject) => {
         pgpool.query('insert into contacts(user_id, firstname, lastname, with_account, bio) values($1, $2, $3, false, $4);', [user_id, fname, lname, bio], (err, res) => {
@@ -170,7 +237,15 @@ var addContact = (user_id, fname, lname, bio) => {
         })
     })
 }
-
+/**
+* @callback addContactwithAccount
+* @desc This function adds the contact to the database under the user's contact lists. However, it also stores the contact's user ID.
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @param {string} fname - Contact's First Name
+* @param {string} lname - Contact's Last Name
+* @param {number} acct_num - Contact's User ID
+* @returns {Promise} Promise object that returns the status of the addition
+*/
 var addContactwithAccount = (user_id, fname, lname, acct_num) => {
     return new Promise((resolve, reject) => {
         pgpool.query('insert into contacts(user_id, firstname, lastname, with_account, acct_num) values($1, $2, $3, true, $4);', [user_id, fname, lname, acct_num], (err, res) => {
@@ -181,7 +256,14 @@ var addContactwithAccount = (user_id, fname, lname, acct_num) => {
         })
     })
 }
-
+/**
+* @callback addContactAddress
+* @desc This function adds the contact's Address to the database
+* @param {number} user_id - User ID to determine which user contacts is needed.
+* @param {number} cont_id - Contact ID to determine which contact to add the address to.
+* @param {string} address - The address itself to be added to the database
+* @returns {Promise} Promise object that returns the status of the addition
+*/
 var addContactAddress = (cont_id, user_id, address) => {
     return new Promise((resolve, reject) => {
         pgpool.query('INSERT INTO contact_address(cont_id, user_id, address) VALUES($1,$2,$3);', [cont_id, user_id, address], (err, res) => {
@@ -199,6 +281,13 @@ var createAccount = (e_mail, password) => {
     })
 }
 //--------------------- User Account Edits And Additions ---------------------
+/**
+* @callback addUserAddress
+* @desc This function adds the user Address to the database
+* @param {number} user_id - User ID to add the Address to.
+* @param {string} address - The address itself to be added to the database
+* @returns {Promise} Promise object that returns the status of the addition
+*/
 var addUserAddress = (user_id, address) => {
     return new Promise((resolve, reject) => {
         pgpool.query('insert into user_address(user_id, address) values($1, $2);', [user_id, address], (err, res) => {
@@ -209,7 +298,14 @@ var addUserAddress = (user_id, address) => {
         })
     })
 }
-
+/**
+* @callback addUserAddress
+* @desc This function adds the user Address to the database
+* @param {number} user_id - User ID to add the Phone Number to.
+* @param {string} phone_number - The phone itself to be added to the database
+* @param {string} type - The type of the phone itself(can be cellular, home or work)
+* @returns {Promise} Promise object that returns the status of the addition
+*/
 var addUserPhone = (user_id, phone_number, type) => {
     return new Promise((resolve, reject) => {
         pgpool.query('insert into user_phone(user_id, p_number, p_type) values($1, $2, $3);', [user_id, phone_number, type], (err, res) => {
@@ -220,7 +316,13 @@ var addUserPhone = (user_id, phone_number, type) => {
         })
     })
 }
-
+/**
+* @callback editUserBio
+* @desc This function updates the user's bio
+* @param {number} user_id - User ID to update the bio.
+* @param {string} new_bio - The new bio to replace the old bio.
+* @returns {Promise} Promise object that returns the status of the addition
+*/
 var editUserBio = (user_id, new_bio) => {
     return new Promise((resolve, reject) => {
         pgpool.query('UPDATE users SET bio = $1 WHERE user_id = $2;', [new_bio, user_id], (err, res) => {
@@ -244,5 +346,8 @@ module.exports = {
     addUserAddress,
     editUserBio,
     addContact,
-    addContactwithAccount
+    addContactwithAccount,
+    getContactAddresses,
+    getContactPhone,
+    getContactsInfo
 }
